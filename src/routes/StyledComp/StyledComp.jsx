a6bafbaede6d6caf6c69409be58b75ef9f330043
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2018-01-23 18:16:03 +0800

    Step 3

--- src/routes/StyledComp/StyledComp.jsx
+++ src/routes/StyledComp/StyledComp.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CustomButton = styled.button`
-  background: green
+  ${props => props.green && 'background: green'};
+  ${props => props.yellow && 'background: yellow'};
+  margin: 20px;
+  padding: 10px;
 `
 
 const StyledComp = () => {
-  return <CustomButton>Styled Component</CustomButton>;
+  return (
+    <div>
+      <CustomButton green>Styled Component 1</CustomButton>
+      <CustomButton yellow>Styled Component 2</CustomButton>
+    </div>
+  );
 }
 
 export default StyledComp
