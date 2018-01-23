b522510da195aa7b93eb4e94ee8d33e469904417
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2018-01-23 17:18:36 +0800

    Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import { Provider } from 'react-redux'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 // INTERNAL DEPENDENCIES
-import { Login, Chat } from 'routes'
 import store from 'store'
 
 const App = () => (
   <Provider store={store}>
     <Router>
       <Fragment>
-        <Route exact path={'/'} component={Login} />
-        <Route exact path={'/chat'} component={Chat} />
+          
       </Fragment>
     </Router>
   </Provider>
