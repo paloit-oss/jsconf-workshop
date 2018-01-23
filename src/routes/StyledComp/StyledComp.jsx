88e0b76316ee3b33a8547ea89ff04d0d9d535ce7
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2018-01-23 17:24:32 +0800

    Step 2

--- src/routes/StyledComp/StyledComp.jsx
+++ src/routes/StyledComp/StyledComp.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CustomButton = styled.button`
+  background: green
+`
 
 const StyledComp = () => {
-  return <button>html button</button>;
+  return <CustomButton>Styled Component</CustomButton>;
 }
 
 export default StyledComp
