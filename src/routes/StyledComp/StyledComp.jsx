561dedac1c201e7d99b46e5f5f6c630cc34fc42a
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2018-01-25 13:31:04 +0800

    Step 5

--- src/routes/StyledComp/StyledComp.jsx
+++ src/routes/StyledComp/StyledComp.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 
-const CustomButton = styled.button`
-  background: ${props => props.theme.primary};
+const buttonBase = props => `
   margin: 20px;
   padding: 10px;
+`
+
+const Button = styled.button`
+  ${buttonBase}
+  background: ${props => props.theme.primary};
+`
+const SuperButton = styled.button`
+  ${buttonBase}
+  background: ${props => props.theme.primary};
+  border-radius: 8px;
 `
 const theme1 = {
   primary: 'green'
@@ -19,10 +28,10 @@ const StyledComp = () => {
   return (
     <div>
       <ThemeProvider theme={theme1}>
-        <CustomButton>Styled Component 1</CustomButton>
+        <Button>Styled Component 1</Button>
       </ThemeProvider>
       <ThemeProvider theme={theme2}>
-        <CustomButton>Styled Component 2</CustomButton>
+        <SuperButton>Styled Component 2</SuperButton>
       </ThemeProvider>
     </div>
   );
